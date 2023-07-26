c8c0d2083cf89120b3b790de99da7c4a861c11b1
Pablo Perdiz <pabloalfayaperdiz@gmail.com>
2023-07-26 01:06:20 -0300

    fix: adding important condition

--- desafioTreinee1.js
+++ desafioTreinee1.js
@@ -1,6 +1,16 @@
 function folhaPagamento(valorHora,horasTrabalhadas) {
     let salarioBruto = valorHora * horasTrabalhadas
-    let impostoRenda = salarioBruto*5/100
+
+    let porcentagemIR = 20
+    if(salarioBruto <= 900){
+        porcentagemIR = 0
+    } else if(salarioBruto <= 1500){
+        porcentagemIR = 5
+    } else if(salarioBruto <= 2500){
+        porcentagemIR = 10
+    }
+
+    let impostoRenda = salarioBruto*porcentagemIR/100
     let inss = salarioBruto/10
     let fgts = salarioBruto*11/100
     let totalDescontos = impostoRenda + inss
@@ -10,7 +20,7 @@ function folhaPagamento(valorHora,horasTrabalhadas) {
 
     Salário bruto (${valorHora.toFixed(2)} * ${horasTrabalhadas})    : R$ ${salarioBruto.toFixed(2)}
 
-    ( - ) IR ( 5% )               : R$ ${impostoRenda.toFixed(2)}
+    ( - ) IR ( ${porcentagemIR}% )               : R$ ${impostoRenda.toFixed(2)}
 
     ( - ) INSS ( 10% )            : R$ ${inss.toFixed(2)}
 
